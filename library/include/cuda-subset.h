a76ac64859fbd92b3b7d486c9bb20a067e5be11f
coldzerofear <coldzerofear@outlook.com>
2026-05-25 23:48:52 +0800

    update library cuda-subset.h

--- library/include/cuda-subset.h
+++ library/include/cuda-subset.h
@@ -1284,8 +1284,10 @@ typedef struct CUDA_KERNEL_NODE_PARAMS_st {
     unsigned int sharedMemBytes; /**< Dynamic shared-memory size per thread block in bytes */
     void **kernelParams;         /**< Array of pointers to kernel parameters */
     void **extra;                /**< Extra options */
-} CUDA_KERNEL_NODE_PARAMS_v1;
-typedef CUDA_KERNEL_NODE_PARAMS_v1 CUDA_KERNEL_NODE_PARAMS;
+    CUkernel kern;               /**< Kernel to launch, will only be referenced if func is NULL */
+    CUcontext ctx;               /**< Context for the kernel task to run in. The value NULL will indicate the current context should be used by the api. This field is ignored if func is set. */
+} CUDA_KERNEL_NODE_PARAMS_v2;
+typedef CUDA_KERNEL_NODE_PARAMS_v2 CUDA_KERNEL_NODE_PARAMS;
 
 /**
  * Memset node parameters
@@ -1378,11 +1380,33 @@ typedef struct CUDA_CONDITIONAL_NODE_PARAMS {
 } CUDA_CONDITIONAL_NODE_PARAMS;
 
 /**
+ * Child graph node ownership
+ */
+typedef enum CUgraphChildGraphNodeOwnership_enum {
+      CU_GRAPH_CHILD_GRAPH_OWNERSHIP_CLONE = 0,     /**< Default behavior for a child graph node. Child graph is cloned
+                                                         into the parent and memory allocation/free nodes can't be present
+                                                         in the child graph. */
+      CU_GRAPH_CHILD_GRAPH_OWNERSHIP_MOVE  = 1,     /**< The child graph is moved to the parent. The handle to the child graph
+                                                         is owned by the parent and will be destroyed when the parent is
+                                                         destroyed.
+
+                                                         The following restrictions apply to child graphs after they have been moved:
+                                                          Cannot be independently instantiated or destroyed;
+                                                          Cannot be added as a child graph of a separate parent graph;
+                                                          Cannot be used as an argument to cuGraphExecUpdate;
+                                                          Cannot have additional memory allocation or free nodes added. */
+} CUgraphChildGraphNodeOwnership;
+
+/**
  * Child graph node parameters
  */
 typedef struct CUDA_CHILD_GRAPH_NODE_PARAMS_st {
     CUgraph graph; /**< The child graph to clone into the node for node creation, or
-                        a handle to the graph owned by the node for node query */
+                        a handle to the graph owned by the node for node query.
+                        The graph must not contain conditional nodes. Graphs
+                        containing memory allocation or memory free nodes must
+                        set the ownership to be moved to the parent. */
+    CUgraphChildGraphNodeOwnership ownership; /**< The ownership relationship of the child graph node. */
 } CUDA_CHILD_GRAPH_NODE_PARAMS;
 
 
